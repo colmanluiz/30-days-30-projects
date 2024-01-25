31645e74a400207d669597a661c60770cca436c4
Luiz Colman <colmanluizz@gmail.com>
2024-01-25 10:32:40 -0300

    function addTask implementada.

--- day 2/script.js
+++ day 2/script.js
@@ -12,6 +12,27 @@ function addTask(event) {
     li.innerHTML = inputBox.value;
     rowList.appendChild(li);
   }
+
+  //TIVE QUE USAR O FOR + ONCLICK, CASO USASSE O FOREACH + EVENTLISTENER BASICAMENTE O QUE ACONTECERIA SERIA QUE TODOS OS ELEMENTOS TERIAM LISTENERS DUPLICADOS. NESSE CASO, OS NUMEROS PARES FUNCIONARIAM E OS IMPARES NÃO. (PARES: SIM -> NAO -> SIM -> NAO, IMPARES: SIM -> NAO -> SIM)
+
+  //EXEMPLO QUE NÃO FUNCIONA (EVENTLISTENER)
+  /* let allLi = document.querySelectorAll("li");
+  for (let x = allLi.length - 1; x >= 0; x--) {
+    allLi[x].addEventListener("click", function inputInfo(event) {
+      const liSelected = event.currentTarget;
+
+      liSelected.classList.toggle("checked");
+    });
+  } */
+
+  let allLi = document.querySelectorAll("li");
+  for (let x = allLi.length - 1; x >= 0; x--) {
+    allLi[x].onclick = function inputInfo(event) {
+      const liSelected = event.currentTarget;
+
+      liSelected.classList.toggle("checked");
+    };
+  }
 }
 
 addButton.addEventListener("click", addTask);
